a00bfd48ffccdd46100148af0b81c863a098eeb1
joona12v <120109936+joona12v@users.noreply.github.com>
2024-04-11 18:30:27 +0300

    Add files via upload

---
+++ app/templates/footer.js
@@ -0,0 +1,21 @@
+import { html } from "lighterhtml";
+import settings from "../settings.json";
+
+export const renderFooter = html.node`<footer class="footer">
+  <div class="tagline">
+    <b>Tulpical X Celex</b><br />
+    
+  </div>
+  <a
+    class="btn--remix"
+    target="_top"
+    href="https://discord.gg/NXEXgVWcpT"
+  >
+    <img
+      src=""
+      alt=""
+    />
+    join our discord.
+  </a>
+</footer>
+`;
